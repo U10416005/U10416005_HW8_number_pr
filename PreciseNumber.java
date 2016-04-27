fff69e8c9eb22bafac57b47eb3ef03b4ab53785a
U10416005 <r3r346@gmail.com>
2016-04-28 02:43:08 +0800

    Write another caculate method
    Write another caculate method:subtract and mutiply

--- PreciseNumber.java
+++ PreciseNumber.java
@@ -4,9 +4,20 @@ import java.math.BigDecimal;
 public class PreciseNumber{
 	public PreciseNumber(){	
 	}
-	public void caculate(String an,String bn){
+	public void caculate(String c,String an,String bn){
 		BigDecimal a = new BigDecimal(an);
 		BigDecimal b = new BigDecimal(bn);
-		System.out.println(a.add(b));
+		if (Integer.valueOf(c)==1){
+			System.out.println(a.add(b));
+		}
+		else if(Integer.valueOf(c)==2){
+			System.out.println(a.subtract(b));
+		}
+		else if(Integer.valueOf(c)==3){
+			System.out.println(a.multiply(b));
+		}
+		else{
+			System.out.println("You do not do the caculate");
+		}
 	}
 }
