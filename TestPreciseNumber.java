fdf89d3a5fc0a261a66a0f60cac5f1c13282feea
U10416005 <r3r346@gmail.com>
2016-04-28 02:44:31 +0800

    Respond the PreciseNumber patch
    Respond the PreciseNumber patch:ask the users what caculate action they want by enter the code

--- TestPreciseNumber.java
+++ TestPreciseNumber.java
@@ -3,10 +3,27 @@ import java.util.Scanner;
 public class TestPreciseNumber{
 	public static void main(String[] args){
 		Scanner input = new Scanner(System.in);
-		System.out.println("Enter two number to do precise add caculate");
-		String a = input.nextLine();
-		String b = input.nextLine();
-		PreciseNumber og = new PreciseNumber();
-		og.caculate(a,b);
+		System.out.println("what caculate do you want to do,press the number,1 is plus,2 is subtract,3 is multiply,and nothing is none");
+		String ch = input.nextLine();
+		while(Integer.valueOf(ch)==1||Integer.valueOf(ch)==2||Integer.valueOf(ch)==3){
+			if (Integer.valueOf(ch)==1){
+				System.out.println("Enter two number to do precise add caculate");
+			}
+			else if(Integer.valueOf(ch)==2){
+				System.out.println("Enter two number to do precise subtract caculate");
+			}
+			else if(Integer.valueOf(ch)==3){
+				System.out.println("Enter two number to do precise mutiply caculate");	
+			}
+			System.out.println("Enter first number");
+			String a = input.nextLine();
+			System.out.println("Enter second number");
+			String b = input.nextLine();
+			PreciseNumber og = new PreciseNumber();
+			og.caculate(ch,a,b);
+			System.out.println("If you want to caculate again,do the same rule:number 1 is plus,2 is subtract,three is mutiply,and other is none");
+			ch = input.nextLine();
+		}
+		System.out.println("You do not do the caculate,end the program");
 	}
 }
