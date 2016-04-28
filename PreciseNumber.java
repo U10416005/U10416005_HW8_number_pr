f495e2b5b7dd06154e5f4d1f271d71e83ec6b85b
U10416005 <r3r346@gmail.com>
2016-04-28 11:01:38 +0800

    Write the last caculate action
    Write the last caculate action:divide,and ask user to enter the precise value

--- PreciseNumber.java
+++ PreciseNumber.java
@@ -1,21 +1,32 @@
 //U10416005ªL«Ø¦t
+//import BigDecimal
 import java.math.BigDecimal;
 
 public class PreciseNumber{
+	//no-arg constructor
 	public PreciseNumber(){	
 	}
-	public void caculate(String c,String an,String bn){
+	//method for caculate
+	public void caculate(String c,String an,String bn,String pre){
 		BigDecimal a = new BigDecimal(an);
 		BigDecimal b = new BigDecimal(bn);
+		//plus case
 		if (Integer.valueOf(c)==1){
 			System.out.println(a.add(b));
 		}
+		//subtract case
 		else if(Integer.valueOf(c)==2){
 			System.out.println(a.subtract(b));
 		}
+		//multiply case
 		else if(Integer.valueOf(c)==3){
 			System.out.println(a.multiply(b));
 		}
+		//divide case
+		else if(Integer.valueOf(c)==4){
+			System.out.println(a.divide(b,Integer.valueOf(pre),BigDecimal.ROUND_HALF_UP));
+		}
+		//none case
 		else{
 			System.out.println("You do not do the caculate");
 		}
